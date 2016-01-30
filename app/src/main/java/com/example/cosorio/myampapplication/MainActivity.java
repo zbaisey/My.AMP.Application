c942ab7c3b9b7a0f489b7325941c7f16123b43be
zbaisey <zahrea.baiseybutler@gmail.com>
2016-01-30 11:55:23 -0700

    changed color

--- app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
+++ app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
@@ -8,16 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private SeekBar textSize = null;
+    private SeekBar second = null;
+    private TextView textToGrow = null;
+    private RatingBar rate = null;
+    private int FiveStars = 5;
+static final String console_text ="AMPconsolestring";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.i(console_text, "Entered onCreate Function");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,10 +37,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+textSize = SeekBar findViewById(R.id.seekBar2);
+    second= SeekBar findViewById(R.id.seekBar);
+    textToGrow= TextView findViewById(R.id.textView3);
+    rate= RatingBar findViewById(R.id.ratingBar);
+    rate.setMax(FiveStars)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        Log.i(console_text, "Entered onCreate Function");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
